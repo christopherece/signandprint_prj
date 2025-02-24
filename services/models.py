1011b474acb8de1a8e2210775c4396919348549d
Chris Ancheta <anchetc@aklc.govt.nz>
2025-02-24 16:27:09 +1300

    Fixed Service Layout

--- services/models.py
+++ services/models.py
@@ -18,5 +18,6 @@ class Service(models.Model):
     price_max = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     usage = models.CharField(max_length=255, blank=True, null=True)
     type = models.CharField(max_length=50, choices=TYPE_CHOICES, default='signage')
+    
     def __str__(self):
         return self.name
