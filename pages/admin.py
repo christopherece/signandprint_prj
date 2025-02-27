234846a6cf2879e21f95f0ac3a1167f8d8521e78
unknown <Administrator@BALAYDALAKAY.LOCAL>
2025-02-28 09:36:15 +1300

    Removed Pages

--- pages/admin.py
+++ pages/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import Contact
+# from .models import Contact
 
-# Register your models here.
-class ContactAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone', 'category', 'subject', 'description')
-    search_fields = ('name', 'email', 'category', 'subject')
-    list_filter = ('category',)
-    verbose_name = "General Inquiry"
-    verbose_name_plural = "General Inquiries"
+# # Register your models here.
+# class ContactAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'email', 'phone', 'category', 'subject', 'description')
+#     search_fields = ('name', 'email', 'category', 'subject')
+#     list_filter = ('category',)
+#     verbose_name = "General Inquiry"
+#     verbose_name_plural = "General Inquiries"
 
-admin.site.register(Contact, ContactAdmin)
+# admin.site.register(Contact, ContactAdmin)
